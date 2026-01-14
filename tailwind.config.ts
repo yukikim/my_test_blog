ee86164c082ca348f78001b3cd633527ffca96e3
yukikim <tki6ra@gmail.com>
2026-01-14 14:20:29 +0900

    microCMSでmd編集に対応のため@tailwindcss/typography導入

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography],
+};
+
+export default config;
